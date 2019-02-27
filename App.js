26638c6da3addc0177d431a97fb3c2e46cd3d599
GUNA ANGS <nguna.mdi@gmail.com>
2019-02-27 23:53:46 +0530

    Navigations added

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends React.Component {
+class FirstScreen extends React.Component {
+
+  static navigationOptions = {
+    title:'Home',
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={() => this.props.navigation.navigate('MainPage')} title='Go to Main Screen' />
       </View>
     );
   }
 }
+
+
+class SecondScreen extends React.Component {
+
+  static navigationOptions = {
+    title:'Main',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={() => this.props.navigation.navigate('Home')} title='Go Back to Home Screen' />
+      </View>
+    );
+  }
+}
+
+const navigationPages = createStackNavigator({
+  Home: { screen: FirstScreen },
+  MainPage: { screen: SecondScreen }
+},
+  {
+    initialRouteName: 'Home'
+  });
+
+const App = createAppContainer(navigationPages);
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
